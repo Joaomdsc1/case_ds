057da9f12889f34881228b03535d5a5fcdad4cb0
Joao Guilherme <jgmarcondes34@gmail.com>
2025-07-28 11:46:30 -0300

    última revisão

--- modelo_10_final.py
+++ modelo_10_final.py
@@ -86,7 +86,7 @@ param_grid = {
 }
 
 # 8. GridSearch com Validação Cruzada
-cv_strategy = StratifiedKFold(n_splits=3, shuffle=True, random_state=42)
+cv_strategy = StratifiedKFold(n_splits=4, shuffle=True, random_state=42)
 grid_search = GridSearchCV(
     pipeline, param_grid, cv=cv_strategy, n_jobs=-1, scoring='roc_auc', verbose=1, return_train_score=True
 )
